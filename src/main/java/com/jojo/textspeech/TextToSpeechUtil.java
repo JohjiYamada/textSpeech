b8029e6ed0db6c7888e9a3e7d147f6d25f9a45cf
Johji Yamada <johji.yamada@scskap.com>
2022-10-06 17:52:29 +0800

    initial

---
+++ src/main/java/com/jojo/textspeech/TextToSpeechUtil.java
@@ -0,0 +1,58 @@
+package com.jojo.textspeech;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import com.google.cloud.texttospeech.v1.AudioConfig;
+import com.google.cloud.texttospeech.v1.AudioEncoding;
+import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
+import com.google.cloud.texttospeech.v1.SynthesisInput;
+import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
+import com.google.cloud.texttospeech.v1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
+import com.google.protobuf.ByteString;
+
+public class TextToSpeechUtil {
+	
+	public static final String PATH = "src/main/resources/static/mp3/";
+	/**
+	 * Demonstrates using the Text to Speech client to synthesize text or ssml.
+	 *
+	 * @param text the raw text to be synthesized. (e.g., "Hello there!")
+	 * @throws Exception on TextToSpeechClient Errors.
+	 */
+	public static ByteString synthesizeText(String text) throws Exception {
+	  // Instantiates a client
+	  try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
+	    // Set the text input to be synthesized
+	    SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+	    // Build the voice request
+	    VoiceSelectionParams voice =
+	        VoiceSelectionParams.newBuilder()
+	            .setLanguageCode("ja-JP") // languageCode = "en_us"
+	            .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
+	            .build();
+
+	    // Select the type of audio file you want returned
+	    AudioConfig audioConfig =
+	        AudioConfig.newBuilder()
+	            .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
+	            .build();
+
+	    // Perform the text-to-speech request
+	    SynthesizeSpeechResponse response =
+	        textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+
+	    // Get the audio contents from the response
+	    ByteString audioContents = response.getAudioContent();
+	    return audioContents;
+//	    // Write the response to the output file.
+//	    try (OutputStream out = new FileOutputStream("output.mp3")) {
+//	      out.write(audioContents.toByteArray());
+//	      System.out.println("Audio content written to file \"output.mp3\"");
+//	      return audioContents;
+//	    }
+	  }
+	}
+}
--- src/main/java/com/jojo/textspeech/controllers/TextSpeechController.java
+++ src/main/java/com/jojo/textspeech/controllers/TextSpeechController.java
@@ -1,13 +1,41 @@
 package com.jojo.textspeech.controllers;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.protobuf.ByteString;
+import com.jojo.textspeech.TextToSpeechUtil;
 
 @Controller
 public class TextSpeechController {
 	
+	private String text = "とりあえずなんでも文字を音声に起こす事はできるようになったよー。";
+	
 	@RequestMapping("/")
-	private String index() {
+	private Object index() throws Exception {
+		
+		ByteString audioContents = TextToSpeechUtil.synthesizeText(text);
+		try (OutputStream out = new FileOutputStream(TextToSpeechUtil.PATH+"output.mp3")) {
+	      out.write(audioContents.toByteArray());
+//	      System.out.println("Audio content written to file \"output.mp3\"");
+	    }
 		return "/index.html";
 	}
+
+//	public static void main(String[] args) throws Exception {
+//		TextSpeechController t = new TextSpeechController();
+//		t.index();
+//	}
+	@GetMapping(value = "/file")
+	public @ResponseBody byte[] getImage() throws Exception {
+		ByteString audioContents = TextToSpeechUtil.synthesizeText(text);
+		return audioContents.toByteArray();
+	}
 }
+
+
